45099acdc5742e18d19e15853cf996714a40c350
aggy17 <munyivak17@gmail.com>
2023-02-16 08:43:52 +0300

    for_loop;even_odd numbers

---
+++ week-2/assignment1.py
@@ -0,0 +1,26 @@
+#for loop
+names=["kelly","Michael","Beyonce","Maria","anita"]
+for n in names:
+    print(n + " are you more stunning than the other?")
+print("We are all equally stunning")
+#to determine even numbers
+for i in range(101):
+    if i%2==0:
+     print(i)
+    else:
+       print(i)
+#totaling even numbers
+total=0
+for e in range(51):
+   if e%2==0:
+     total+=e
+     print(total) 
+#for both even and odd numbers
+for n in range(101):
+   if n%2==0:
+      print(n,"even number")
+   else:
+      print(n,"odd numbers")
+
+
+
